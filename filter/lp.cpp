1228ef35e4e8e018ff4bcc89c46ec1eaea4b349f
zhoujian <zhoujian.ww@gmail.com>
2022-12-17 12:53:04 +0800

    lp  apply config

--- filter/lp.cpp
+++ filter/lp.cpp
@@ -5,30 +5,31 @@
 namespace wibot::control
 {
 
-	static float wrap(float x, float w)
-	{
-		return x - 2 * w * floor((x + w) / 2 / w);
-	}
-	//template<typename T>
-	void FirstOrderLowPassFilter::apply_config()
-	{
-		_alpha = config.sample_time / (config.sample_time + 1.0f / config.cutoff_freq / 2.0f / _PI);
-		_1_alpha = 1 - _alpha;
-	};
+    static float wrap(float x, float w)
+    {
+        return x - 2 * w * floor((x + w) / 2 / w);
+    }
+    //template<typename T>
+    Result FirstOrderLowPassFilter::apply_config()
+    {
+        _alpha = config.sample_time / (config.sample_time + 1.0f / config.cutoff_freq / 2.0f / _PI);
+        _1_alpha = 1 - _alpha;
+        return Result::OK;
+    };
 
-	//template<typename T>
-	float FirstOrderLowPassFilter::filter(float input)
-	{
-		if (!config.enable_wrap)
-		{
-			_outputLast = _alpha * input + _1_alpha * _outputLast;
-		}
-		else
-		{
-			_outputLast = wrap(_outputLast + _alpha * wrap(input - _outputLast, config.wrap_value), config.wrap_value);
-		}
+    //template<typename T>
+    float FirstOrderLowPassFilter::filter(float input)
+    {
+        if (!config.enable_wrap)
+        {
+            _outputLast = _alpha * input + _1_alpha * _outputLast;
+        }
+        else
+        {
+            _outputLast = wrap(_outputLast + _alpha * wrap(input - _outputLast, config.wrap_value), config.wrap_value);
+        }
 
-		return _outputLast;
-	};
+        return _outputLast;
+    };
 
 } // namespace wibot::control
--- filter/lp.hpp
+++ filter/lp.hpp
@@ -4,32 +4,32 @@
 #include "base.hpp"
 namespace wibot::control
 {
-	struct FirstOrderLowPassFilterConfig
-	{
-		float sample_time;
-		float cutoff_freq;
-		bool enable_wrap = false;
-		float wrap_value;
-	};
-	//template<typename T>
-	class FirstOrderLowPassFilter : public Configurable<FirstOrderLowPassFilterConfig>
-	{
-	 public:
+    struct FirstOrderLowPassFilterConfig
+    {
+        float sample_time;
+        float cutoff_freq;
+        bool enable_wrap = false;
+        float wrap_value;
+    };
+    //template<typename T>
+    class FirstOrderLowPassFilter : public Configurable<FirstOrderLowPassFilterConfig>
+    {
+     public:
 
-		void apply_config() override;
+        Result apply_config() override;
 
-		float filter(float input);
+        float filter(float input);
 
-	 private:
-		float _alpha;
-		float _1_alpha;
+     private:
+        float _alpha;
+        float _1_alpha;
 
-		float _outputLast;
-	};
+        float _outputLast;
+    };
 
-	// using FirstOrderLowPassFilterf = FirstOrderLowPassFilter<float>;
-	// using FirstOrderLowPassFilters = FirstOrderLowPassFilter<uint16_t>;
-	// using FirstOrderLowPassFilterl = FirstOrderLowPassFilter<uint32_t>;
+    // using FirstOrderLowPassFilterf = FirstOrderLowPassFilter<float>;
+    // using FirstOrderLowPassFilters = FirstOrderLowPassFilter<uint16_t>;
+    // using FirstOrderLowPassFilterl = FirstOrderLowPassFilter<uint32_t>;
 } // namespace wibot::control
 
 #endif // __WWCONTROL_LP_HPP__
